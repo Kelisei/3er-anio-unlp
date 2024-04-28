e77f9c3300bfe54f86b5b3f110c85b854a4a1a70
Francisco Percara <99842611+Kelisei@users.noreply.github.com>
2024-04-28 16:33:03 -0300

    Cambios

--- Objetos2/ejercicios/ejercicio13/src/main/java/ejercicios/ejercicio10/Similares.java
+++ Objetos2/ejercicios/ejercicio13/src/main/java/ejercicios/ejercicio10/Similares.java
@@ -5,6 +5,6 @@ import java.util.List;
 public class Similares extends Sugerencia {
     @Override
     protected List<Pelicula> sugerirPeliculas(Decodificador decodificador) {
-        return decodificador.getGrilla().stream().flatMap(p -> p.getSimilares().stream()).distinct().toList();
+        return decodificador.getGrilla().stream().flatMap(p -> p.getSimilares().stream()).distinct().sorted((p1, p2) -> p1.getAnioDeEstreno().compareTo(p2.getAnioDeEstreno())).toList();
     }
 }
