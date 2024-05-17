edc0dbf122d502717746cdfaa61b7ed989278643
Francisco Percara <99842611+Kelisei@users.noreply.github.com>
2024-05-17 17:52:07 -0300

    20

---
+++ Objetos2/ejercicios/ejercicio20/src/main/java/ar/edu/info/oo2/filtros/imageFilters/Dull.java
@@ -0,0 +1,13 @@
+package ar.edu.info.oo2.filtros.imageFilters;
+
+import java.awt.image.BufferedImage;
+
+public class Dull extends Filter {
+  
+  
+    
+  public BufferedImage filter(BufferedImage image) {
+    
+    return image;
+  }
+}
