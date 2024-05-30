f3989982f5c93a1f4a8b3c41dffa92448331d9ba
Francisco Percara <frankestudios71@gmail.com>
2024-05-30 17:31:00 -0300

    Descarga del codigo

---
+++ Objetos2/Frameworks/Ejercicio 1- Java Logging/A/2/src/main/java/ar/edu/unlp/oo1/ejercicio1/app/Ejercicio1Application.java
@@ -0,0 +1,24 @@
+package ar.edu.unlp.oo1.ejercicio1.app;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+
+import javax.swing.SwingUtilities;
+
+import ar.edu.unlp.oo1.ejercicio1.ui.WallPostUI;
+
+public class Ejercicio1Application {
+
+	
+	
+	public static void main(String[] args) throws SecurityException, IOException {
+
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new WallPostUI();
+			}
+		});
+	}
+
+}
